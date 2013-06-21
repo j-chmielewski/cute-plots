13a8dcc2647248f466ac09eeb961c48f5371b8fb
Jacek Chmielewski <jacek.chmielewski@teonite.com>
2013-06-22 00:10:45 +0200

    cutie 1: ax.plot(r, i*r**2*sin(r), i*r**2*cos(r))

---
+++ cutie1.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import arange, sin, cos
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+r = arange(0, 40, .1)
+for i in range(0, 10):
+    ax.plot(r, i*r**2*sin(r), i*r**2*cos(r))
+plt.show()
