1e8f29c8bc6865996787ad7b7af2beb4c6f2eccf
Jacek Chmielewski <jacek.chmielewski@teonite.com>
2013-06-23 09:10:05 +0200

    surf 2: transparency

--- surf2.py
+++ surf2.py
@@ -14,7 +14,7 @@ X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=True)
+                       linewidth=0, antialiased=True, alpha=.5)
 ax.set_zlim(-1.01, 1.01)
 
 ax.zaxis.set_major_locator(LinearLocator(10))
